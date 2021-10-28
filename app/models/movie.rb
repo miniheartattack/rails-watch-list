c79e2d2013a56b4c5b82fd0efcd0d34a45edaaf5
Abdel Ben <benabdelhak1@hotmail.com>
2021-10-28 16:55:28 +0100

    commit challenge2 validations part

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
 has_many :bookmarks
 validates :title, uniqueness: true, presence: true
-validates :rating, numericality: { only_integer: boolean_value }
-validates :bookmark, dependent: :destroy
+validates :overview, presence: true
 end
