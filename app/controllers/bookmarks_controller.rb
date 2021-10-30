0b0a5f034a9a03664db4e2e3dd06c7e3e3c8a47b
Abdel Ben <benabdelhak1@hotmail.com>
2021-10-30 10:33:41 +0100

    cleaning up my git status

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,5 @@
 class BookmarksController < ApplicationController
 
-  def new
-    @bookmark = Bookmark.new
-  end
 
-  def create
-        @bookmark = Bookmark.create(params[:restaurant])
-        @bookmark.save
-        redirect_to controllers_path
-  end
+
 end
